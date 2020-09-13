1435ea2d5c3d81d0c637d9ffec45f22b7574cf5e
Daniel Hammer <HammerAPI@gmail.com>
2020-09-13 19:24:53 -0400

    Initalized repo. Moved material from CCG over

---
+++ graph_coloring/base_game/game.py
@@ -0,0 +1,5 @@
+class Game:
+    def __init__(self, graph, player_1, player_2):
+        self.graph = graph
+        self.player_1 = player_1
+        self.player_2 = player_2
---
+++ graph_coloring/base_game/player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, graph, num_colors, strategy):
+        self.graph = graph
+        self.num_colors = num_colors
+        self.strategy = strategy

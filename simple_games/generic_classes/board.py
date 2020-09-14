f99ce7601973f82f4f1bb548c49bb06ac40f5b6b
Daniel Hammer <HammerAPI@gmail.com>
2020-09-13 20:45:09 -0400

    Removed bloat

---
+++ simple_games/generic_classes/board.py
@@ -0,0 +1,7 @@
+class Board:
+    def __init__(self):
+        self.move_set = []
+        self.state_set = []
+
+    def update(self):
+        pass
---
+++ simple_games/generic_classes/referee.py
@@ -0,0 +1,9 @@
+class Referee:
+    def __init__(self):
+        pass
+
+    def is_legal(self, state_set, move_set):
+        pass
+
+    def is_winning(self, state_set):
+        pass

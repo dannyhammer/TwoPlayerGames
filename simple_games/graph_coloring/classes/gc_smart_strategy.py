df2099039367196a4f5419b69bf3b0f48886a34b
Daniel Hammer <HammerAPI@gmail.com>
2021-02-27 20:28:52 -0500

    feat: Added a 'smart' strategy that uses supplied vertex and color orderings to determine a move

---
+++ simple_games/graph_coloring/classes/gc_smart_strategy.py
@@ -0,0 +1,55 @@
+##
+# Trainable Strategy
+# Attempts to follow the vertex coloring provided by the `data` parameter.
+# The `data` parameter is a dictionary consisting of a vertex ordering and a color ordering.
+#
+# Authors: Daniel Hammer, Nicholas O'Kelley, Andrew Penland, Andrew Shelton
+#
+# Date: 2021-02-27
+##
+
+from random import choice
+
+import sys
+sys.path.append("../..")
+from generic_classes.strategy_interface import StrategyInterface
+
+class GCSmartStrategy(StrategyInterface):
+
+    def __init__(self, name, data = None):
+        """
+        Strategy constructor.
+
+        Args:
+            name : The name of the strategy
+            data : (Optional) Data to read from to influence moves.
+        """
+        self.name = name
+        self.data = data
+
+    def move(self, board):
+        """
+        Attempts to make a move by following the vertex coloring provided by
+        the `data` field.
+
+        Args:
+            board : The board being played on
+
+        Return:
+            A proposed move.
+        """
+        # Iterate over the vertex ordering
+        for vtx in self.data["vertices"]:
+            # If the vertex is uncolored
+            if board.state[vtx]["color"] == 0:
+                # Get the vertex's neighbors' colors
+                adj_colors = [board.state[adj]["color"] for adj in board.state[vtx]["adj"]]
+
+                # Iterate over the color ordering
+                for color in self.data["colors"]:
+
+                    # If the color is available, use it
+                    if color not in adj_colors:
+                        return {"vertex": vtx, "color": color}
+
+        return None
